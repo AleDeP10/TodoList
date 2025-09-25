55cc2a64331880dc8d806418964850a064df9146
AleDeP10 <alessandro.deprato@outlook.com>
2025-09-25 15:29:47 +0200

    Fix to Modal display in horizonal device

--- todolist-fe-nextjs/src/lib/components/ui/Modal.tsx
+++ todolist-fe-nextjs/src/lib/components/ui/Modal.tsx
@@ -41,9 +41,9 @@ export default function Modal({
   const modalContent = (
     <div
       onClick={handleBackdropClick}
-      className="fixed inset-0 z-50 bg-black/40 backdrop-blur-md flex items-center justify-center px-4 py-6 overflow-y-auto scroll-py-6"
+      className="fixed inset-0 z-50 bg-black/40 backdrop-blur-md flex justify-center items-center px-4 py-6 overflow-y-auto scroll-py-6"
     >
-      <div className="bg-[var(--modal-bg)] text-[var(--fg)] rounded-lg shadow-lg w-full max-w-lg overflow-y-auto">
+      <div className="bg-[var(--modal-bg)] text-[var(--fg)] rounded-lg shadow-lg w-full max-w-lg">
         {/* 🔘 Header */}
         <div className="flex justify-between items-center px-6 py-4 border-b border-[var(--fg)]">
           <h2 className="text-lg font-semibold w-full text-center whitespace-normal">{title}</h2>
